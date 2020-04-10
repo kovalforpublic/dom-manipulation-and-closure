8fb392f3b68fc3ffc02d8fdae2dbaa719aeb610c
kovalforpublic <kovalforpublic@gmail.com>
2020-04-10 14:06:22 +0300

    refactored (got rid of useless wrappers + renamed methods)

--- src/inputGenerator.js
+++ src/inputGenerator.js
@@ -6,48 +6,44 @@ function InputHandler(className) {
     const backButton = document.querySelector(className + ' > button[name=back]');
     const forwardButton = document.querySelector(className + ' > button[name=forward]');
 
-    function addValue(newValue) {
-        history.push(newValue);
+    function addValue() {
+        history.push(input.value);
         currentIndex = history.length - 1;
+        checkForwardDisability();
+        checkBackDisability();
     }
     function next() {
         const lastElementIndex = history.length - 1;
         if (currentIndex < lastElementIndex) {
             currentIndex += 1;
             input.value = history[currentIndex];
-            isForwardDisabled();
+            checkForwardDisability();
         }
-        isBackDisabled();
+        checkBackDisability();
     }
 
     function previous() {
         if (currentIndex !== 0) {
             currentIndex -= 1;
             input.value = history[currentIndex];
-            isBackDisabled();
+            checkBackDisability();
         }
-        isForwardDisabled();
+        checkForwardDisability();
     }
-    
-    function isForwardDisabled() {
+
+    function checkForwardDisability() {
         const lastElementIndex = history.length - 1;
         forwardButton.disabled = currentIndex === lastElementIndex ? true : false;
     }
-    function isBackDisabled() {
+    function checkBackDisability() {
         backButton.disabled = currentIndex === 0;
     }
 
     (() => {
-        input.addEventListener('input', () => {
-            addValue(input.value);
-        })
+        input.addEventListener('input', addValue)
     })();
     (() => {
-        backButton.addEventListener('click', () => {
-            previous();
-        })
-        forwardButton.addEventListener('click', () => {
-            next();
-        })
+        backButton.addEventListener('click', previous);
+        forwardButton.addEventListener('click', next);
     })();
 }
